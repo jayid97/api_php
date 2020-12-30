3c56d0bf5112a293dc147a96019be3764584a4f3
Muhamad Jayid Muntaz Bin Saad <43310381+jayid97@users.noreply.github.com>
2020-12-30 14:14:15 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 // required headers
 if (isset($_SERVER['HTTP_ORIGIN'])) {
 
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST");
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
